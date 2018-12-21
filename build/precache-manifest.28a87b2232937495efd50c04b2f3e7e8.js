928dc87c5a2cd0e9ea4891b0cd5a16940a9d5586
Torre Matthew <torre.matthew@gmail.com>
2018-12-20 20:24:05 -0500

    reset game on 3 incorrect guesses

---
+++ build/precache-manifest.28a87b2232937495efd50c04b2f3e7e8.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "493e1f321c4cba85ffe7",
+    "url": "/ReactGame/static/js/runtime~main.493e1f32.js"
+  },
+  {
+    "revision": "518ee3f0617c2749d869",
+    "url": "/ReactGame/static/js/main.518ee3f0.chunk.js"
+  },
+  {
+    "revision": "324b6cf3b56bf43378ce",
+    "url": "/ReactGame/static/js/1.324b6cf3.chunk.js"
+  },
+  {
+    "revision": "cf2fa38320c1c66184f12f052423b1d0",
+    "url": "/ReactGame/index.html"
+  }
+];
